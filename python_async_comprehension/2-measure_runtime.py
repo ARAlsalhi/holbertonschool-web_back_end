8886eee8cfe59b9f6b19920971ab33062f07aeb3
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-29 22:47:14 +0300

    T2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
-"""This module measures concurrent async comprehension execution"""
-
+"""Module that measures the runtime of executing async_comprehension
+four times in parallel."""
 import asyncio
 import time
 
-async_comprehension = __import__(
-    '1-async_comprehension'
-).async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Return the runtime of four concurrent async comprehensions"""
-    start_time = time.perf_counter()
-
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
-    return time.perf_counter() - start_time
+    """Execute async_comprehension four times in parallel
+    and return the total runtime."""
+    start = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return time.perf_counter() - start
