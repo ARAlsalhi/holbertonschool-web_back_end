3f591b0c1a3e461c4e333fb07f87662183da1ffc
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-29 22:04:22 +0300

    T0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""This module provides a function for adding two floats"""
 
 def add(a: float, b: float) -> float:
+    """Return the sum of two floating-point numbers"""
     return a + b
