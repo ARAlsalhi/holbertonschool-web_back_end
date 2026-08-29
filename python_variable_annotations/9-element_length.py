a0e051220e52f54a7ce5983993611d2abf918776
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-29 22:29:40 +0300

    T9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-"""This module provides a function for concatenating two strings"""
+"""This module provides a function for measuring sequence elements"""
+
+from typing import Iterable, List, Sequence, Tuple
 
 
-def concat(str1: str, str2: str) -> str:
-    """Return the concatenation of two strings"""
-    return str1 + str2
+def element_length(
+        lst: Iterable[Sequence]
+        ) -> List[Tuple[Sequence, int]]:
+    """Return each sequence with its corresponding length"""
+    return [(i, len(i)) for i in lst]
